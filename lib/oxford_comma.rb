9511f74695941dd611f4759da676f978f896ecf3
Kolton Starr <starrkolton@gmail.com>
2019-04-30 17:33:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,19 @@
 def oxford_comma(array)
-  new = array.join(" and ")
-  nuarray = new.split(" ")
-  nuarray.delete_at(1)
-  nuarray.insert(1, ",")
+  if array.length == 1
+    new = array.join(" and ")
+  else 
+    new = array.join(" and ")
+    nuarray = new.split(" ")
+    nuarray.delete_at(1)
+    nuarray.insert(1, ",")
   
   
   
   
   
   
-  a = nuarray[0] + nuarray[1]
-  b = nuarray[2] + " " + nuarray[3] + " " + nuarray[4]
-  a + " " + b
+    a = nuarray[0] + nuarray[1]
+    b = nuarray[2] + " " + nuarray[3] + " " + nuarray[4]
+    a + " " + b
+  end
 end
