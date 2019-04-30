c3b305013ce5c1197081c1f869a199f4e46558cc
Kolton Starr <starrkolton@gmail.com>
2019-04-30 18:01:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  if array.length == 1
+    new = array.join(" and ")
+  elsif array.length == 2 
+    new = array.join(" and ")
+  elsif array.length == 3
+    new = array.join(" and ")
+    nuarray = new.split(" ")
+    nuarray.delete_at(1)
+    nuarray.insert(1, ",")
+  
+    a = nuarray[0] + nuarray[1] #Kiwi,
+    b = nuarray[2] + "," + " " + nuarray[3] + " " + nuarray[4]
+    a + " " + b
+    
+  else 
+    last_item = array.pop
 
+    mod_array = array.join(", ") 
+
+    final_array = mod_array + "," + " " + "and" + " " + last_item
+  end
 end
