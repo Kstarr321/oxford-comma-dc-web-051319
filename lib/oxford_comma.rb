5ba7b8523552e0fe95e52eacd471fd8fe4d7f8f8
Kolton Starr <starrkolton@gmail.com>
2019-04-30 17:30:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,15 @@
 def oxford_comma(array)
   new = array.join(" and ")
+  nuarray = new.split(" ")
+  nuarray.delete_at(1)
+  nuarray.insert(1, ",")
   
+  
+  
+  
+  
+  
+  a = nuarray[0] + nuarray[1]
+  b = nuarray[2] + " " + nuarray[3] + " " + nuarray[4]
+  a + " " + b
 end
